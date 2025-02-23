34aba00e3f81fb0a62af96f8d55bc3a0d4865689
devaiwa <devaiwa@users.noreply.github.com>
2025-02-23 22:14:35 +0000

    added setup and arguments

--- secondsetup.sh
+++ secondsetup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+## Setup
 sudo NEEDRESTARTMODE=a apt update && sudo apt upgrade -y
+
+##Getting Gala Node software
 sudo wget --trust-server-names https://links.gala.com/DownloadLinuxNode
 
 shopt -s nullglob
@@ -7,6 +10,26 @@ list=(~/gala-node*)
 ## echo "${list[0]}"
 sudo tar xzvf ${list[0]}
 
+## Installing
+
 yes Y | sudo gala-node/install.sh
+
+## Setting API-KEY (second argument in line)
+sudo gala-node config api-key ${2}
+
+licenses=`sudo gala-node licenses`
+
+IFS=', ' read -r -a array <<< "$licenses"
+
+for element in "${array[@]}"
+do
+    sudo gala-node workload add $element
+done
+
+## Naming the Node (first argument in line)
+sudo gala-node rename ${1}
+
+## Starting the node
+sudo gala-node start 
 
 echo "Secondary Setup finished"
