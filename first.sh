65869248b464d2378bbea0a34c47b0ab8415a460
devAiwa <devaiwa@gmail.com>
2025-02-25 01:40:28 +0200

    added vm tools.

--- first.sh
+++ first.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 ## Version 1.0
 
+sudo rm -f /etc/machine-id
+sudo dbus-uuidgen --ensure=/etc/machine-id
+sudo rm /var/lib/dbus/machine-id
+sudo dbus-uuidgen --ensure
+
 ## Hostname
 gettingip=`hostname -I`
 IFS='. ' read -r -a array <<< "$gettingip"
 ip=${array[3]}
 
 ipall="$(hostname -I | cut -d" " -f1 | xargs)"
-
-
-
 
 hostname="galanode-$ip-$NODEID"
 echo "Hostname: $hostname"
@@ -19,10 +21,9 @@ echo "$ipall  $hostname" >> /etc/hosts
 
 ## Docker Setup
 //sudo NEEDRESTARTMODE=a apt update && sudo apt upgrade -y
+//sudo apt-get install virtualbox-guest-additions-iso -y
 //sudo apt install docker.io -y
 //sudo systemctl enable --now docker
-
-## sudo apt-get install virtualbox-guest-additions-iso -y
 
 ## Rebooting    
 ## sudo reboot
