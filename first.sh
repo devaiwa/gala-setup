7f5b636c0524427c917c92c1cf459867f68bdd69
devAiwa <devaiwa@gmail.com>
2025-02-25 01:07:03 +0200

    added hosts

--- first.sh
+++ first.sh
@@ -5,14 +5,24 @@
 gettingip=`hostname -I`
 IFS='. ' read -r -a array <<< "$gettingip"
 ip=${array[3]}
+
+ipall="$(hostname -I | cut -d" " -f1 | xargs)"
+
+
+
+
 hostname="galanode-$ip-$NODEID"
 echo "Hostname: $hostname"
 sudo hostnamectl set-hostname $hostname
+echo "$ipall  $hostname" >> /etc/hosts
+
 
 ## Docker Setup
-sudo NEEDRESTARTMODE=a apt update && sudo apt upgrade -y
-sudo apt install docker.io -y
-sudo systemctl enable --now docker
+//sudo NEEDRESTARTMODE=a apt update && sudo apt upgrade -y
+//sudo apt install docker.io -y
+//sudo systemctl enable --now docker
+
+## sudo apt-get install virtualbox-guest-additions-iso -y
 
 ## Rebooting    
 ## sudo reboot
